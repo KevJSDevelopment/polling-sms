3b1dbb36aa6f18772c61b8ef33e74e17c3cd76d7
KevJSDevelopment <Kshhn12@gmail.com>
2024-08-08 14:42:56 -0400

    init commit

---
+++ Polling-Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Polling_Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Polling-Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Polling_Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
